3e08ea445c67e6c48208b552385675d92b356ea3
TimurIskandarov <tim-darov@rambler.ru>
2020-03-16 21:31:02 +0400

    feat: slide-1 and mixer section portfolio

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Navigation Bar
+
 // Найдём первый элемент с классом navigation на странице (навигационная панель)
 let nav = document.querySelector(".navigation");
 
@@ -20,3 +22,48 @@ nav.addEventListener('click', function(event) {
     // ...и добавим его той, по которой выполнен клик
     target.classList.add('active');
 })
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+
+// Portfolio
+
+const tagsNav = document.querySelector('.tags');
+tagsNav.addEventListener('click', (event) => {
+    let target = event.target;
+    let notActive = !target.classList.contains('tag');
+    if (notActive) { return; }
+    filterPictures();    
+    for(let link of document.querySelectorAll('.tag')){
+        link.classList.remove('portfolio_active'); 
+    }
+    target.classList.add('portfolio_active');   
+})
+
+const filterPictures = () => {
+    let img = {};
+    let arr = [0,1,2,3,4,5,6,7,8,9,10,11];
+    var shuffledPictures = arr.sort(function(){
+        return Math.random() - 0.5;
+    });
+    const gallery = document.querySelectorAll(".portfolio__grid .portfolio__picture");
+    for(const [index, pic] of gallery.entries()){
+        let tmp = pic.querySelector('.pic');
+        img[index] = tmp;
+        tmp.remove();
+    }
+    gallery.forEach((el, i) => {
+        el.append(img[shuffledPictures[i]]);        
+    })    
+}
+
+const setPictures = document.querySelector(".portfolio__grid");
+setPictures.addEventListener('click', (event) => {
+    let target = event.target;
+    let notActive = !target.classList.contains('pic');
+    if (notActive) { return; }
+    for(let link of document.querySelectorAll(".pic")){
+        link.classList.remove('pic_active'); 
+    }   
+    event.target.classList.add('pic_active');
+    // }
+})
+
